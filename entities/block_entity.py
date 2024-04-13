fdc5fde6951cd8bdfed7c0926bbdfc11209a97dc
Pawel Szewczuk <pawel.szewczuk.h@gmail.com>
2024-04-13 21:35:52 +0200

    Block generation refactor

--- entities/block_entity.py
+++ entities/block_entity.py
@@ -1,8 +1,6 @@
+from dataclasses import dataclass
+from typing import List
 
-class Block():
-    def __init__(self, first_block_id, second_block_id, third_block_id, fourth_block_id):
-        
-        self.first_block_id = first_block_id
-        self.second_block_id = second_block_id
-        self.third_block_id = third_block_id
-        self.fourth_block_id = fourth_block_id
+@dataclass
+class Block:
+    block_ids: List[int]
