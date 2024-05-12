e3ae22e6a4628124f7b95810d4f8cc7440572b81
ptakpiotr <piter020@outlook.com>
2024-05-12 23:47:44 +0200

    Instruction from PDF not webpage

--- views/instruction_view.py
+++ views/instruction_view.py
@@ -1,24 +1,42 @@
+import io
 import tkinter as tk
-import tkinterweb
-
+import fitz
+import os
+from pathlib import Path
 from controls.page_button import PageButton
 from views.base_view import BaseView
+from PIL import Image, ImageTk
 
 
 class InstructionView(BaseView):
     def __init__(self, controller):
         super().__init__(controller)
 
+    def retrieve_file_absolute_path(self, file_name = "doc.pdf"):
+        if os.path.exists(file_name):
+            path = f'{Path().absolute()}/{file_name}'
+
+            return path
+        else:
+            raise FileNotFoundError()
+
+    def show_pdf(self, path, page_number, canvas):
+        self.retrieve_file_absolute_path(path)
+        self.pdf_document = fitz.open(path)
+        self.page = self.pdf_document.load_page(page_number)
+        pix = self.page.get_pixmap()
+        img_data = pix.tobytes("ppm")
+        self.img = Image.open(io.BytesIO(img_data))
+        self.photo = ImageTk.PhotoImage(image=self.img)
+        canvas.create_image(0, 0, image=self.photo, anchor='nw')
+
     def __add_widgets(self):
         back_button = PageButton(
             self, text="Home", command=self.controller.back_to_main)
-        frame = tkinterweb.HtmlFrame(self, messages_enabled=False)
-
-        # load a website
-        frame.load_website(
-            "https://github.com/PZ-Tetris/tetris/blob/master/README.md")
+        canvas = tk.Canvas(self, width=550, height=600)
+        self.show_pdf('doc.pdf', 1, canvas)
         back_button.grid(column=0, row=0)
-        frame.grid(column=0, row=1, columnspan=2)
+        canvas.grid(column=0, row=1)
 
     def present(self):
         self.__add_widgets()
