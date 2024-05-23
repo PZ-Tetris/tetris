10edf2914bfd66aa3ef13ab2e87b93f1f08efd25
krygose <krzysztof1400@gmail.com>
2024-05-23 14:31:07 +0200

    automatyczna detekcja kwadratów z przejściem

--- square_tests.py
+++ square_tests.py
@@ -2,73 +2,122 @@ import cv2
 import numpy as np
 from PIL import ImageGrab
 from screeninfo import get_monitors
-from app import App
-from views.game_view import GameView
-from views.base_view import BaseView
-import tkinter as tk
-
-
+import subprocess
+import threading
+import time
+import pyautogui
 
 def get_screen_resolution():
     primary_monitor = get_monitors()[0]
     return primary_monitor.width, primary_monitor.height
 
-def draw_square(image, square):
-    cv2.drawContours(image, [square], 0, (255, 192, 203), 5)
-
-def find_squares(min_area, max_area):
+def change_view_to_game():
     screen_width, screen_height = get_screen_resolution()
-
     print("Capturing screen...")
-
-    screen = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
-
-
-    gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-    
-    
-
-    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    
-    gauss1 = cv2.GaussianBlur(gray,(5,5),0)
-    cann = cv2.Canny(gauss1,100,200)
-    # cv2.imshow('Square Detection', cann)
-    # cv2.waitKey(0)
-
-    print("Finding contours...")
-    contours, _ = cv2.findContours(cann, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    screen2 = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
+    screen2 = cv2.cvtColor(screen2, cv2.COLOR_BGR2RGB)
+    play_img = cv2.imread('play.jpg', cv2.IMREAD_COLOR)
+    result = cv2.matchTemplate(screen2, play_img, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    print(max_loc)
+    print(max_val)
+    w = play_img.shape[1]
+    h = play_img.shape[0]
+    cv2.rectangle(screen2, max_loc, (max_loc[0] + w, max_loc[1] + h), (0, 255, 255), 2)
+    pyautogui.moveTo(max_loc)
+    pyautogui.click()
 
 
-    for contour in contours:
+def find_best_match(screen, images):
+    best_match_index = -1
+    best_match_value = float('-inf')  # Ustawiamy na wartość ujemną nieskończoną
+    best_match_loc = None
 
-        area = cv2.contourArea(contour)
+    for i, img in enumerate(images):
+        result = cv2.matchTemplate(screen, img, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, max_loc = cv2.minMaxLoc(result)
 
-        if min_area < area < max_area:
-            epsilon = 0.02 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
+        if max_val > best_match_value:
+            best_match_index = i
+            best_match_value = max_val
+            best_match_loc = max_loc
+
+    if best_match_index != -1:
+        best_match = images[best_match_index]
+        if best_match_loc is not None:
+            h, w = best_match.shape[:-1]
+            cv2.rectangle(screen, best_match_loc, (best_match_loc[0] + w, best_match_loc[1] + h), (255, 255, 255), 4)
+
+        return best_match, best_match_loc, best_match_value
+    else:
+        return None, None, None
+def find_squere():
+    screen_width, screen_height = get_screen_resolution()
+    print("Capturing screen...")
+    screen2 = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
+    screen2 = cv2.cvtColor(screen2, cv2.COLOR_BGR2RGB)
+    squere_img = [
+        cv2.imread('yellow.jpg', cv2.IMREAD_COLOR),
+        cv2.imread('blue.jpg', cv2.IMREAD_COLOR),
+        cv2.imread('green.jpg', cv2.IMREAD_COLOR),
+        cv2.imread('red.jpg', cv2.IMREAD_COLOR),
+        cv2.imread('cyan.jpg', cv2.IMREAD_COLOR),
+        cv2.imread('orange.jpg', cv2.IMREAD_COLOR),
+        cv2.imread('purple.jpg', cv2.IMREAD_COLOR)
+    ]
+    best_match, match_loc, match_val = find_best_match(screen2, squere_img)
+    if best_match is not None:
+        print("Najlepsze dopasowanie znalezione!")
+        print("Współrzędne maksymalnej wartości:", match_loc)
+        print("Wartość dopasowania:", match_val)
+    else:
+        print("Nie znaleziono dopasowania.")
+
+    # Wyświetlenie obrazu z zaznaczonym dopasowaniem
+    cv2.imshow('Dopasowanie', screen2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-            if len(approx) == 4:
-                return screen, approx
-        
-# print(sys.path)
 
-# baseView = BaseView(tk.Frame)
-# app = GameView()
-mainloop()
+def run_app():
+    # Uruchomienie innego skryptu
+    relative_path = 'app.py'
+    subprocess.Popen(['python', relative_path])
 
 
 
+# Wczytaj obraz za pomocą OpenCV
+image = cv2.imread('play.jpg')
 
-min_area = 100
-max_area = 50000
+# Sprawdź pierwszy piksel (0, 0)
+(b, g, r) = image[0, 0]
 
-print("Looking for squares...")
+if r > b:
+    print("Obraz jest w formacie BGR")
+else:
+    print("Obraz jest w formacie RGB")
 
-screen, square = find_squares(min_area, max_area)
 
-draw_square(screen, square)
+# Uruchomienie skryptu app.py jako osobny proces
+run_app()
 
-cv2.imshow('Square Detection', screen)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Pauza na 1 sekundę
+time.sleep(1)
+
+# Utworzenie wątku dla funkcji change_view_to_game
+thread1 = threading.Thread(target=change_view_to_game)
+
+
+# Start wątku
+thread1.start()
+
+# Czekanie na zakończenie wątku
+thread1.join()
+
+time.sleep(1)
+thread2 = threading.Thread(target=find_squere())
+thread2.start()
+
+thread2.join()
+
